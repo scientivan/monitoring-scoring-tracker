3a757e813a372a182254959b82b0acdc272cc676
scientivan <dienmuhammad030406@gmail.com>
2026-04-29 02:40:56 +0700

    Refactor code structure for improved readability and maintainability

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./app/core/swagger");
+const apiRoutes = require("./app/presentation/routes");
+
+const app = express();
+const port = 3300;
+const HOST = "0.0.0.0"; //biar bisa diakses dari docker
+
+// Middleware untuk parsing JSON
+app.use(express.json());
+
+// Setup endpoint untuk Swagger UI
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Daftarkan routes aplikasi
+app.use("/api/v1", apiRoutes);
+
+app.listen(port, HOST, () => {
+  console.log(`🚀 Server berjalan di http://${HOST}:${port}`);
+  console.log(
+    `📚 Dokumentasi Swagger tersedia di http://${HOST}:${port}/api-docs`,
+  );
+});
