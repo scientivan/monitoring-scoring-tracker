4bd41d6b3817d44b59498999f03ca46bc5ad1625
scientivan <dienmuhammad030406@gmail.com>
2026-05-06 01:08:38 +0700

    fix: update server port configuration and docker-compose port mapping

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const swaggerSpec = require("./app/core/swagger");
 const apiRoutes = require("./app/presentation/routes");
 
 const app = express();
-const port = 3300;
+const port = process.env.PORT || 8080;
 const HOST = "0.0.0.0"; //biar bisa diakses dari docker
 
 // Middleware untuk parsing JSON
